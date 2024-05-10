fea67ca09046bbdf3cc27fc4b17a83b138f65b18
Abdelrahman Soliman <89320683+3bdo72@users.noreply.github.com>
2024-05-10 23:22:45 +0300

    feat: :sparkles: Add Sequence to Bookstore modul
    add sequence to the fiction book model

--- custom-addons/book-store/models/fiction_book.py
+++ custom-addons/book-store/models/fiction_book.py
@@ -5,5 +5,13 @@ class FictionBook(models.Model):
     _name = "fiction"
     _description = "fiction book"
 
+    ref = fields.Char(default = "New", readonly=True)
     name = fields.Char(string="Book Name")
     author = fields.Char(string="Author Name")
+
+    @api.model
+    def create(self, vals):
+        res = super(FictionBook, self).create(vals)
+        if res.ref == 'New':
+            res.ref = self.env["ir.sequence"].next_by_code("fiction_ref")
+        return res
