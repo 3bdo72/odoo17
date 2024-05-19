7395c98af21c128f5cce52b5d67f47f26b7add91
Abdelrahman Soliman <89320683+3bdo72@users.noreply.github.com>
2024-05-20 00:46:30 +0300

    feat: :sparkles: Add Phone Fields With A Regex Validation

--- custom-addons/hospital_management/models/patient.py
+++ custom-addons/hospital_management/models/patient.py
@@ -1,19 +1,44 @@
 from odoo import models, fields, api
+import re # for the regex
+from odoo.exceptions import ValidationError
+
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
 
-    ref = fields.Char(string="Reference", default = "New", readonly=True)
-    name = fields.Char()
+    ref = fields.Char(string="Reference", default="New", readonly=True)
+    name = fields.Char(required=True)
     age = fields.Integer()
+    date_of_birth = fields.Date()
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], default='male')
     active = fields.Boolean(default=True)
-    # doctor = fields.Many2one('hospital.doctor', ondelete='set null', index=True, null=True)
+    address = fields.Text()
+    phone = fields.Char(string='Phone', default='+20 ', required=True)
+    home_phone = fields.Char(string='Home Phone', default='03 ', required=True)
+    email = fields.Char()
 
     @api.model
     def create(self, vals):
-        res = super(HospitalPatient, self).create(vals)
-        if res.ref == 'New':
-            res.ref = self.env["ir.sequence"].next_by_code("patient_ref")
-        return res
+        if vals.get('ref', 'New') == 'New':
+            vals['ref'] = self.env['ir.sequence'].next_by_code('patient_ref') or 'New'
+        return super(HospitalPatient, self).create(vals)
+
+        # Another way of creating sequence number 
+    # @api.model
+    # def create(self, vals):
+    #     res = super(HospitalPatient, self).create(vals)
+    #     if res.ref == 'New':
+    #         res.ref = self.env["ir.sequence"].next_by_code("patient_ref")
+    #     return res
+
+    @api.constrains('phone', 'home_phone')
+    def _check_phone_numbers(self):
+        phone_pattern = re.compile(r'^\+20 \d{10}$')
+        home_phone_pattern = re.compile(r'^03 \d{7}$')
+        
+        for record in self:
+            if record.phone and not phone_pattern.match(record.phone):
+                raise ValidationError("Phone number must be in the format +20 XXXXXXXXXX (e.g., +20 1063097699).")
+            if record.home_phone and not home_phone_pattern.match(record.home_phone):
+                raise ValidationError("Home phone number must be in the format 03 XXXXXXX (e.g., 03 5411457).")
